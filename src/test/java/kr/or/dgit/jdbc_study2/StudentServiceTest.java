648b8843260e441d3514727f8f42d094b8488e3d
ldy <tlgurdlsn37@naver.com>
2018-02-13 10:00:36 +0900

    master

--- src/test/java/kr/or/dgit/jdbc_study2/StudentServiceTest.java
+++ src/test/java/kr/or/dgit/jdbc_study2/StudentServiceTest.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.junit.After;
 import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
 import kr.or.dgit.jdbc_study2.dto.Student;
 import kr.or.dgit.jdbc_study2.service.StudentService;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class StudentServiceTest {
 	 private static StudentService service;
 
@@ -23,11 +26,16 @@ public class StudentServiceTest {
 	   }
 
 	   @Test
-	   public void testFindAllStudents() {
+	   public void testAFindAllStudents() {
 	      List<Student> list = service.findAllStudents();
 	      
 	      //Assert.isNonEmpty(list);
 	   }
 	
-	
+	   @Test
+	   public void testBFindAllStudents() {
+	      List<Student> list = service.findAllStudents();
+	      
+	      //Assert.isNonEmpty(list);
+	   }
 }
